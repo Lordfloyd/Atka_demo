1a3a25c1d1cfe0ad64f5d17d47359788e5a83655
Lordfloyd <paulmussy@gmail.com>
2020-12-07 13:27:04 +0100

    updated SimpleLockFund.sol

--- contracts/SimpleLockFund.sol
+++ contracts/SimpleLockFund.sol
@@ -2,13 +2,15 @@ pragma solidity >=0.4.22 <0.8.0;
 
 contract SimpleLockFund {
     
-    enum State {Locked, Release}
+    enum State {Created, Locked, Release}
     State public state;
 
     struct FundData {
+        address payable fundCustodian;
         uint balance;
-        uint releaseBlock;
+        uint releaseTime;
     }
+    
     mapping (address => FundData) funds;
 
     modifier inState(State _state) {
@@ -18,36 +20,54 @@ contract SimpleLockFund {
         );
         _;
     }
-
-    modifier onlyAfter() {
-        require(block.timestamp > funds[msg.sender].releaseBlock,"Lock Time is not ended yet" );
+    
+     modifier onlyAfter() {
+        require(block.timestamp > funds[msg.sender].releaseTime , "Rent is not ended yet");
+        _;
+    }
+    
+    modifier onlyFundCustodian () {
+        require(msg.sender == funds[msg.sender].fundCustodian, "You don't have the right for this action !");
         _;
     }
 
-    function cashIn() public payable {
-        uint amount = msg.value;
-        address payable sender = msg.sender;
-        
-        if(msg.sender.balance > amount ) {
-            state = State.Release;
-            msg.sender.transfer(amount);
+    function getContractBalance() external view returns (uint) {
+        return address(this).balance;
+    }
 
+    function cashIn(uint lockTimeS)public payable {
+
+        uint amount = msg.value;
+        if (funds[msg.sender].balance > 0)
+            
+            msg.sender.transfer(msg.value);
+        else {
+            state = State.Locked;
+            funds[msg.sender].fundCustodian = msg.sender;
+            funds[msg.sender].balance = amount;
+            funds[msg.sender].releaseTime = now + lockTimeS;
+        }
+    }
+    
+    
+    function cashOut() public onlyAfter onlyFundCustodian {
+        if (funds[msg.sender].balance != 0) {
+            state = State.Release;
+            msg.sender.transfer(funds[msg.sender].balance);
+            funds[msg.sender].balance = 0;
+            funds[msg.sender].releaseTime = 0;
         }
     }
 
-    function cashOut() public payable onlyAfter() {
-        state = State.Release;
-
-        msg.sender.transfer(funds[msg.sender].balance);
-    }
-
-
-    function getFundLock () external view returns (uint x) {
+    function getMyLockedFunds() public view onlyFundCustodian returns (uint x) {
         return funds[msg.sender].balance;
     }
-
-    function getLockTime () external view returns (uint x) {
-        return funds[msg.sender].releaseBlock;
+    
+    function getMyLockedFundsReleaseTime() public view onlyFundCustodian returns (uint x) {
+	    return funds[msg.sender].releaseTime;
     }
 
+    function getNow() public view returns (uint x) {
+        return block.timestamp;
+    }
 }
